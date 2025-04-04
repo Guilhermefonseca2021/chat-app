7e0ae1363744fa81ad7dfdff089f177a398c85f1
guilhermefonseca2021 <gsbloogs194@gmail.com>
2025-04-04 13:05:38 -0300

    testing user reading to database

---
+++ src/database/_schema.ts
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import pool from './pool'
+import { DatabaseError } from 'pg';
+
+// read schema.sql
+const schema = fs.readFileSync(path.resolve(__dirname, './schema.sql'), {
+    encoding: 'utf8',
+});
+
+(async () => {
+    try {
+        await pool.query(schema);
+        console.log('✅ Reset Database!');
+    } catch (err) {
+        console.error('❌ Error resetting database:', err);
+    } finally {
+        await pool.end();
+    }
+})();
--- src/database/pool.ts
+++ src/database/pool.ts
@@ -1,4 +1,15 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+// dotenv.config();
+const port = process.env.DB_PORT
+const NumberPORT = parseInt(port as string);
 
-dotenv.config();
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: NumberPORT,
+    application_name: process.env.DB_NAME
+})
+
+export default pool;
